beb2307ac3cf8476d713fc3f92d849b7195e9645
Srishty Jain <jainsrishty309@gmail.com>
2026-01-14 17:38:43 +0000

    add

---
+++ variable.js
@@ -0,0 +1,14 @@
+const accountID = 445878
+let accountemail = "srishty@outlook.com"
+var accountpassword = "12345"
+accountcity = "delhi"
+let acountstate;
+
+
+//accountId = 12458 (not allowed)
+
+/* 
+avoide the use of var beacuse of block scope and functional scope
+*/
+
+console.table([accountID, accountemail, accountpassword, accountcity])
