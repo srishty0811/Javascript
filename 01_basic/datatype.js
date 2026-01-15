9ba74e32c4881689e4bf44f3b6d3e7e27784a637
Srishty Jain <jainsrishty309@gmail.com>
2026-01-15 17:36:29 +0000

    add

---
+++ 01_basic/datatype.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as a newer version
+
+// alert(3+3) (we are using nodejs not browser)
+
+// code readability should be high
+
+let name = "srishty"
+let age = 19
+let isloggedIN = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+//boolean => true/false
+//null => standalone value
+// undefined
+// symbol
+//object 
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
